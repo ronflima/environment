ac36a4e3d4670580f28e634bed41bb1c53365f45
Ronaldo Faria Lima <ronaldo.faria.lima@gmail.com>
2020-04-24 08:29:27 -0300

    Fixes for correct modeline on chtmkt python skeleton.

--- emacs.d/skeletons/python-skel.el
+++ emacs.d/skeletons/python-skel.el
@@ -32,11 +32,11 @@
   "Python Chiclete Mkt file"
   \n
   > "# -*- mode:python;coding:utf-8 -*-" \n
-  > "#" \n
-  > "# Copyright (c)" _ (format-time-string "%Y" (current-time)) _ " - Chicletemkt - All Rights Reserved" \n
-  > "# Created: " _ (format-time-string "%Y-%m-%d" (current-time)) _ " by " _ (user-full-name) _ \n
-  > "#" \n
-  > "# This file purpose: " _ (skeleton-read "This file purpose: ") _ \n
-  > "#" _ \n
+  > "\"\"\"" \n 
+  > "Copyright (c)" _ (format-time-string "%Y" (current-time)) _ " - Chicletemkt - All Rights Reserved" \n
+  > "Created: " _ (format-time-string "%Y-%m-%d" (current-time)) _ " by " _ (user-full-name) _ \n
+  >  \n
+  > "This file purpose: " _ (skeleton-read "This file purpose: ") _ \n
+  > "\"\"\"" _ \n
  )
 
