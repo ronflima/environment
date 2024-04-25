bc89460353b07f0ecd947023b5d8af2c3bd31624
Ronaldo Faria Lima <ronaldo.faria-lima@synd.io>
2024-04-25 18:48:07 -0300

    Moved customizations to last in configuration loading.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,10 +1,9 @@
 ;; Emacs Customizations
 ;; Author: Ronaldo F. Lima <ronaldo@brazuca.dev>
 
+(load-library (expand-file-name "preferences.el" user-emacs-directory))
 (setq custom-file (expand-file-name "customizations.el" user-emacs-directory))
 (when 
        (file-exists-p custom-file)
        (load custom-file)
 )
-
-(load-library (expand-file-name "preferences.el" user-emacs-directory))
