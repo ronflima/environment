26d05ff310eb0a1537f2efbd9d5a5d68a8e4153a
Ronaldo Faria Lima <ronaldo@brazuca.dev>
2025-02-05 18:15:41 -0300

    Adjustments to a django html template

--- emacs.d/skeletons/django-skel.el
+++ emacs.d/skeletons/django-skel.el
@@ -1,15 +1,13 @@
 (define-skeleton django-html-template
   "Django HTML template"
   \n
-  > "{% extends \"base.html\" %}" \n
-  > "{% comment %}" \n
-  > "*****" \n
-  > "# Copyright (c)" (format-time-string "%Y" (current-time)) _ " - " _ user-company-name _ " - All Rights Reserved" \n 
-  > "#" \n
-  > "# Created: " _ (format-time-string "%Y-%m-%d" (current-time))_ " by " _ (user-full-name) _ "<" _ user-mail-address _ ">" _ \n
-  > "#" \n
-  > "# This file purpose: " (skeleton-read "This file purpose: ") \n
-  > "*****" \n \n
-  > "{% endcomment %}" \n \n
-  > "{% load i18n %}" \n
+  "{% extends \"base.html\" %}" \n
+  "{% comment %}" \n
+  "*****" \n
+  "Copyright (c)" (format-time-string "%Y" (current-time)) " - " user-company-name " - All Rights Reserved" \n \n
+  "Created: "  (format-time-string "%Y-%m-%d" (current-time)) " by " (user-full-name) " - (" user-mail-address ")" \n
+  "This file purpose: " (skeleton-read "This file purpose: ") \n
+  "*****" \n \n
+  "{% endcomment %}" \n \n
+  "{% load i18n %}" \n
  )
