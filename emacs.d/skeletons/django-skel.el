643a29ee3dae9ce8c65ed5702e71f5985711ba50
Ronaldo Faria Lima <ronaldo@brazuca.dev>
2025-02-06 10:52:07 -0300

    Fixed a typo

--- emacs.d/skeletons/django-skel.el
+++ emacs.d/skeletons/django-skel.el
@@ -20,7 +20,7 @@
   > "{% include \"form.html\" %}" \n
   > "</div>" \n
   > "<div class=\"container-fluid mt-2 text-center\">" \n
-  > "<button class=\"btn btn-primary\" type=\"submit\">{% translate \"" (skeleton read "Button label: ") "\" %}</button>" \n
+  > "<button class=\"btn btn-primary\" type=\"submit\">{% translate \"" (skeleton-read "Button label: ") "\" %}</button>" \n
   > "</div>" \n
   > "</form>" \n
   )
