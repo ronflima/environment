a6aa8cb577078e776fc12ef09ae0e498bd378172
Ronaldo Faria Lima <ronaldo@brazuca.dev>
2025-02-24 17:06:08 -0300

    Fixes for the org mode skeleton for timesheets

--- emacs.d/skeletons/org-skel.el
+++ emacs.d/skeletons/org-skel.el
@@ -11,7 +11,7 @@
 (defun brazuca-workdays()
   "Outputs a list of working days in Org Mode format"
   (let ((weekday '("Monday" "Tuesday" "Wednesday" "Thursday" "Friday")))
-        (setq activity-plan (mapc (lambda (d) (insert (concat "** " d "\n*** Activity Plan\n"))) weekday))
+        (setq activity-plan (mapc (lambda (d) (insert (concat "** " d "\n\n*** Activity Plan\n\n\n"))) weekday))
         (apply 'concat activity-plan)))
 
 (define-skeleton brazuca-weekly-timesheet
@@ -25,15 +25,15 @@
   "#+OPTIONS: toc:nil" \n \n
   "* Time Report" \n \n
   "This time report is for the following period:" \n \n
-  "- Starting at " (skeleton-read "Starting date: ") \n
-  "- Ending at " (skeleton-read "End date: ") \n \n
+  "- Starting at " \n
+  "- Ending at " \n \n
   "The following table summarizes the worked hours." \n
-  "#+BEGIN: clocktable :scope file :maxlevel 2 :header \"+NAME: clocktable\n\"" \n
+  "#+BEGIN: clocktable :scope file :maxlevel 2 :header \"+NAME: clocktable\\n\"" \n
   "#+CAPTION: Time Summary" \n
   "#+END:" \n
   \n \n
   "** Billable hours" \n
-  "#+TBLNAME billablehours"
+  "#+TBLNAME:billablehours" \n
   "|-" \n
   "|Description|Time|" \n
   "|-" \n
