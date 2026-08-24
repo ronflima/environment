591a9fd48bd31e0ba4e4f2396ebc7d55d6b10e49
Ronaldo Faria Lima <ronaldo@sparta.local>
2026-08-24 17:31:14 -0300

    Added dape support for python debugging inside devcontainers

--- emacs.d/preferences.el
+++ emacs.d/preferences.el
@@ -167,7 +167,6 @@
   :config
   (add-to-list 'eglot-server-programs
                '(python-mode . ("pyright-langserver" "--stdio"))))
-
 ;;
 ;; Python Preferences
 ;;
@@ -187,7 +186,23 @@
   :demand t
   :after python
   :hook ((python-mode . python-black-on-save-mode)))
-
+(use-package dape
+  :ensure t
+  :config
+  ;; Devcontainer configs
+  (add-to-list 'dape-configs
+               `(python-devcontainer
+                 modes (python-mode python-ts-mode)
+                 ensure (lambda (config)
+                          ;; Sobrescreve a porta/conexão do depurador
+                          dape-ensure-command-selected)
+                 command "devcontainer"
+                 command-args ("exec" "--workspace-folder" "." "python" "-m" "debugpy" "--listen" "5678" "--wait-for-client")
+                 host "localhost"
+                 port 5678
+                 :type "python"
+                 :request "attach"
+                 :pathMappings [(:localRoot dape-cwd :remoteRoot "/workspace")])))
 ;;
 ;; Golang preferences
 ;;
