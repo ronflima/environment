d56a8b9f4d36fd4c7e603f224738a41161d62b06
Ronaldo Faria Lima <ronaldo@sparta.local>
2026-09-06 19:42:18 -0300

    Added more settings for pythong development

--- emacs.d/customizations/custom-python.el
+++ emacs.d/customizations/custom-python.el
@@ -38,7 +38,6 @@
   :hook ((python-mode . pyvenv-auto-run)))
 (use-package pyconf
   :ensure t)
-(defalias 'workon 'pyvenv-workon)
 (use-package python-black
   :ensure t
   :demand t
@@ -60,51 +59,4 @@
                  :type "python"
                  :request "attach"
                  :pathMappings [(:localRoot dape-cwd :remoteRoot "/workspace")])))
-
-;; (use-package lsp-mode :ensure t :config (setq warning-minimum-level ':error))
-;; (use-package lsp-pyright :after lsp-mode :ensure t)
-;; (with-eval-after-load 'lsp-mode
-;;   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.venv\\'"))
-;; (use-package lsp-ui
-;;   :ensure t
-;;   :after lsp-mode)
-;; (use-package lsp-pyright
-;;   :after lsp-mode
-;;   :ensure t)
-;; (use-package flycheck
-;;   :after lsp-mode
-;;   :ensure t
-;;   :config (global-flycheck-mode))
-;; (use-package company
-;;   :after lsp-pyright
-;;   :ensure t
-;;   :hook (prog-mode . company-mode)
-;;   :bind
-;;   (:map company-active-map
-;;         ("<tab>" . company-complete-selection))
-;;   (:map lsp-mode-map
-;;         ("<tab>" . company-indent-or-complete-common))
-;;   :custom
-;;   (company-minimum-prefix-length 1)
-;;   (company-idle-delay 0.0))
-;; (use-package company-jedi
-;;   :after company
-;;   :ensure t)
-;; (with-eval-after-load 'flycheck
-;;   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
-
-;; (use-package py-isort :ensure t)
-;; (use-package pyvenv-auto :ensure t :hook ((python-mode . pyvenv-auto-run)))
-
-;; (add-hook 'python-mode-hook 'lsp)
-;; (add-hook 'python-mode-hook 'hs-minor-mode)
-;; (add-hook 'python-mode-hook 'display-line-numbers-mode)
-;; (add-hook 'before-save-hook 'py-isort-before-save)
-
-;; (setq py-autopep8-options '("--max-line-length=132"))
-;; (setq python-fill-docstring-style 'django)
-;; (setq python-indent-offset 4)
-;; (setq dired-guess-shell-alist-user
-;;       '(("^manage.py$" "python * runserver")
-;;         ("\\.py$" "python")
-;;         ("^requirements.txt$" "pip install -r")))
+(use-package virtualenv :ensure t)
