7de58f2d8b2bae7001f16df9ebc8b280b4fbd4c5
Ronaldo Faria Lima <ronaldo@brazuca.dev>
2026-09-06 12:16:34 -0300

    Moved mermaid changes to the right place

--- emacs.d/customizations/custom-packages.el
+++ emacs.d/customizations/custom-packages.el
@@ -35,8 +35,6 @@
 (use-package ssh :ensure t)
 (use-package graphviz-dot-mode :ensure t)
 (use-package markdown-mode :ensure t)
-(use-package mermaid-mode :ensure t)
-(use-package ob-mermaid :ensure t :after mermaid-mode)
 (use-package web-mode :ensure t :config
   (defun brazuca-mode-hook ()
     "Hooks for Web mode."
