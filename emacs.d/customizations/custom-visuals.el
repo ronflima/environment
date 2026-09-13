1e352f0b2840f3cba12d0c9f9c318c347b9b20da
Ronaldo Faria Lima <ronaldo@sparta.local>
2026-09-13 09:22:14 -0300

    Added smal customization for fill columns

--- emacs.d/customizations/custom-visuals.el
+++ emacs.d/customizations/custom-visuals.el
@@ -49,3 +49,5 @@
 (tool-bar-mode 0)
 (turn-on-font-lock)
 (set-scroll-bar-mode 'right)
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+(add-hook 'text-mode-hook #'display-fill-column-indicator-mode)
